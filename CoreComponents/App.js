91ceb31115f5c6395cc683e645e9de1355d73e6c
abanoub-refaat <abanoubref3at18@gmail.com>
2025-01-29 12:42:36 +0200

    Learning Text, Image, and ImageBackground tags

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,14 +1,18 @@
-import { View } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+
+const logoImage = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      {/* <Image source={logoImage} style={{ width: 200, height: 200 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/200/300?grayscale" }}
+        style={{ width: 200, height: 300 }}
+      /> */}
+      <ImageBackground source={logoImage} style={{ flex: 1 }}>
+        <Text>Image text</Text>
+      </ImageBackground>
     </View>
   );
 }
