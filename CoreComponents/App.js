360d9f7077c0e03eb4a0fb665d14d58431a69ae1
abanoub-refaat <abanoubref3at18@gmail.com>
2025-02-01 20:31:56 +0200

    Learning ActivityIndicator component and Alert component/api

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -6,26 +6,53 @@ import {
   Button,
   Pressable,
   StatusBar,
+  ActivityIndicator,
   Modal,
+  Alert,
 } from "react-native";
 import { useState } from "react";
 
 const logoImage = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  const [iStatusVisible, setStatusVisibility] = useState(false);
+  const [istatusVisible, setStatusVisibility] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+    <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+      <ActivityIndicator
+        size={"large"}
+        color={"black"}
+        animating={!istatusVisible}
+      />
       <StatusBar
         backgroundColor="white"
         barStyle="dark-content"
-        hidden={iStatusVisible}
+        hidden={istatusVisible}
       />
       <ScrollView>
         <Button
           title="Show/Hide the Status Bar"
           color="black"
-          onPress={() => setStatusVisibility(!iStatusVisible)}
+          onPress={() => setStatusVisibility(!istatusVisible)}
+        />
+        <Button
+          title={"Alert"}
+          color="black"
+          onPress={() =>
+            Alert.alert(
+              "Hi from the Alert Component!",
+              "Hello! this is an Optional msg!",
+              [
+                {
+                  text: "Cancel",
+                  onPress: () => console.log("Cancel Pressed"),
+                },
+                {
+                  text: "OK",
+                  onPress: () => console.log("OK Pressed"),
+                },
+              ]
+            )
+          }
         />
       </ScrollView>
     </View>
